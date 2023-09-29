6410f62fdd64f4ae50099d698a6b819f06a17d1e
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-09-29 20:59:04 +0200

    add enemies

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
@@ -9,6 +10,12 @@ WIDTH = 1200
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
+COLOR_BLUE = (66, 135, 245)
+COLOR_CRIMSON = (245, 66, 129)
+COLOR_GREEN = (71, 171, 5)
+COLOR_YELLOW = (255, 201, 23)
+COLOR_RED = (186, 12, 0)
+
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
 PLAYER_SIZE = (20, 20)
@@ -18,11 +25,26 @@ player.fill(COLOR_WHITE)
 
 position = player.get_rect()
 
+ENEMY_SIZE = (30, 30)
+ENEMY_CREATION_INTERVAL = 1500 # ms
+ENEMY_COLORS = [COLOR_BLUE, COLOR_CRIMSON, COLOR_GREEN, COLOR_YELLOW, COLOR_RED]
+
+def create_enemy():
+    enemy = pygame.Surface(ENEMY_SIZE)
+    enemy.fill(random.choice(ENEMY_COLORS))
+    position = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *ENEMY_SIZE)
+
+    return [enemy, position]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, ENEMY_CREATION_INTERVAL)
+
 STEP_DOWN = [0, 1]
 STEP_UP = [0, -1]
 STEP_RIGHT = [1, 0]
 STEP_LEFT = [-1, 0]
 
+enemies = []
 FPS = 2000
 in_progress = True
 
@@ -32,6 +54,8 @@ while in_progress:
     for event in pygame.event.get():
         if event.type == QUIT:
             in_progress = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
     
     main_display.fill(COLOR_BLACK)
 
@@ -50,8 +74,11 @@ while in_progress:
     if keys[K_LEFT] and position.left != 0:
         player_step = STEP_LEFT
 
-    main_display.blit(player, position)
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(STEP_LEFT)
+        main_display.blit(enemy[0], enemy[1])
 
+    main_display.blit(player, position)
     position = position.move(player_step)
 
     pygame.display.flip()
