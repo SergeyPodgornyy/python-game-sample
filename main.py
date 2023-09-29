1045e75f80fd2a7c0e6e56e317b54f30a5b862b5
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-09-29 21:28:07 +0200

    add bonuses

--- main.py
+++ main.py
@@ -26,18 +26,34 @@ player.fill(COLOR_WHITE)
 position = player.get_rect()
 
 ENEMY_SIZE = (30, 30)
-ENEMY_CREATION_INTERVAL = 1500 # ms
-ENEMY_COLORS = [COLOR_BLUE, COLOR_CRIMSON, COLOR_GREEN, COLOR_YELLOW, COLOR_RED]
+ENEMY_CREATION_INTERVAL = 750 # ms
+ENEMY_COLORS = [COLOR_BLUE, COLOR_GREEN, COLOR_RED]
 
 def create_enemy():
     enemy = pygame.Surface(ENEMY_SIZE)
     enemy.fill(random.choice(ENEMY_COLORS))
-    position = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *ENEMY_SIZE)
+    position = pygame.Rect(WIDTH, random.randint(0, HEIGHT - 30), *ENEMY_SIZE)
+    step = [random.randint(-3, -1), 0]
 
-    return [enemy, position]
+    return [enemy, position, step]
+
+BONUS_SIZE = (15, 15)
+BONUS_CREATION_INTERVAL = 1500 # ms
+BONUS_COLORS = [COLOR_CRIMSON, COLOR_YELLOW]
+
+def create_bonus():
+    bonus = pygame.Surface(BONUS_SIZE)
+    bonus.fill(random.choice(BONUS_COLORS))
+    position = pygame.Rect(random.randint(0, WIDTH - 15), 0, *BONUS_SIZE)
+    step = [0, random.randint(1, 2)]
+
+    return [bonus, position, step]
 
 CREATE_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY, ENEMY_CREATION_INTERVAL)
+
+CREATE_BONUS = CREATE_ENEMY + 1
+pygame.time.set_timer(CREATE_BONUS, BONUS_CREATION_INTERVAL)
 
 STEP_DOWN = [0, 1]
 STEP_UP = [0, -1]
@@ -45,7 +61,8 @@ STEP_RIGHT = [1, 0]
 STEP_LEFT = [-1, 0]
 
 enemies = []
-FPS = 2000
+bonuses = []
+FPS = 500
 in_progress = True
 
 while in_progress:
@@ -56,6 +73,8 @@ while in_progress:
             in_progress = False
         if event.type == CREATE_ENEMY:
             enemies.append(create_enemy())
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
     
     main_display.fill(COLOR_BLACK)
 
@@ -75,10 +94,23 @@ while in_progress:
         player_step = STEP_LEFT
 
     for enemy in enemies:
-        enemy[1] = enemy[1].move(STEP_LEFT)
+        enemy[1] = enemy[1].move(enemy[2])
         main_display.blit(enemy[0], enemy[1])
+
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(bonus[2])
+        main_display.blit(bonus[0], bonus[1])
+
+        if bonus[1].bottom > HEIGHT:
+            bonuses.pop(bonuses.index(bonus))
 
     main_display.blit(player, position)
     position = position.move(player_step)
+
+    print("Enemies count: ", len(enemies))
+    print("Bonuses count: ", len(bonuses))
 
     pygame.display.flip()
