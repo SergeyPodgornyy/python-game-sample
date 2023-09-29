05c6dc50fbf19b39b6a99118a05025a8b0558b69
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-09-29 20:12:52 +0200

    add interaction to player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
 
 pygame.init()
 
@@ -17,7 +17,11 @@ player = pygame.Surface(PLAYER_SIZE)
 player.fill(COLOR_WHITE)
 
 position = player.get_rect()
-player_step = [1, 1]
+
+STEP_DOWN = [0, 1]
+STEP_UP = [0, -1]
+STEP_RIGHT = [1, 0]
+STEP_LEFT = [-1, 0]
 
 FPS = 2000
 in_progress = True
@@ -31,11 +35,20 @@ while in_progress:
     
     main_display.fill(COLOR_BLACK)
 
-    if position.bottom == HEIGHT or position.top == -1:
-        player_step[1] = -player_step[1]
+    keys = pygame.key.get_pressed()
+    player_step = [0, 0]
 
-    if position.right == WIDTH or position.left == -1:
-        player_step[0] = -player_step[0]
+    if keys[K_DOWN] and position.bottom != HEIGHT:
+        player_step = STEP_DOWN
+
+    if keys[K_UP] and position.top != 0:
+        player_step = STEP_UP
+
+    if keys[K_RIGHT] and position.right != WIDTH:
+        player_step = STEP_RIGHT
+
+    if keys[K_LEFT] and position.left != 0:
+        player_step = STEP_LEFT
 
     main_display.blit(player, position)
 
