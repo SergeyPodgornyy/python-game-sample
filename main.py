f841e94bf51cc7c91f97bde5064e153ade127805
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-10-01 13:33:02 +0200

    allow to continue game after gameover

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import os
 import pygame
 
-from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT, K_SPACE
 
 pygame.init()
 
@@ -11,13 +11,8 @@ WIDTH = 1200
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
-COLOR_BLUE = (66, 135, 245)
-COLOR_CRIMSON = (245, 66, 129)
-COLOR_GREEN = (71, 171, 5)
-COLOR_YELLOW = (255, 201, 23)
-COLOR_RED = (186, 12, 0)
-
-FONT = pygame.font.SysFont('Verdana', 20)
+FONT_SIZE = 20
+FONT = pygame.font.SysFont('Verdana', FONT_SIZE)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -32,12 +27,15 @@ player_image_index = 0
 
 player = pygame.image.load(os.path.join(PLAYER_IMAGE_PATH, PLAYER_IMAGES[player_image_index])).convert_alpha()
 
-position = pygame.Rect(
-    250,
-    (HEIGHT - player.get_height()) / 2,
-    player.get_width(),
-    player.get_height()
-)
+def player_initial_position():
+    return pygame.Rect(
+        250,
+        (HEIGHT - player.get_height()) / 2,
+        player.get_width(),
+        player.get_height()
+    )
+
+position = player_initial_position()
 
 def create_enemy():
     enemy = pygame.image.load('./images/enemy.png').convert_alpha()
@@ -84,6 +82,7 @@ bonuses = []
 score = 0
 FPS = 500
 in_progress = True
+game_over = False
 
 while in_progress:
     pygame.time.Clock().tick(FPS)
@@ -91,59 +90,74 @@ while in_progress:
     for event in pygame.event.get():
         if event.type == QUIT:
             in_progress = False
-        if event.type == CREATE_ENEMY:
-            enemies.append(create_enemy())
-        if event.type == CREATE_BONUS:
-            bonuses.append(create_bonus())
-        if event.type == CHANGE_PLAYER_IMAGE:
-            player_image_index += 1
+        if game_over == False:
+            if event.type == CREATE_ENEMY:
+                enemies.append(create_enemy())
+            if event.type == CREATE_BONUS:
+                bonuses.append(create_bonus())
+            if event.type == CHANGE_PLAYER_IMAGE:
+                player_image_index += 1
 
-            if player_image_index == len(PLAYER_IMAGES):
-                player_image_index = 0
+                if player_image_index == len(PLAYER_IMAGES):
+                    player_image_index = 0
 
-            player = pygame.image.load(os.path.join(PLAYER_IMAGE_PATH, PLAYER_IMAGES[player_image_index])).convert_alpha()
-    
-    bg_X1 -= BACKGROUND_STEP
-    bg_X2 -= BACKGROUND_STEP
+                player = pygame.image.load(os.path.join(PLAYER_IMAGE_PATH, PLAYER_IMAGES[player_image_index])).convert_alpha()
 
-    if bg_X1 < -BACKGROUND.get_width():
-        bg_X1 = BACKGROUND.get_width()
-
-    if bg_X2 < -BACKGROUND.get_width():
-        bg_X2 = BACKGROUND.get_width()
-
-    main_display.blit(BACKGROUND, (bg_X1, 0))
-    main_display.blit(BACKGROUND, (bg_X2, 0))
-
-    keys = pygame.key.get_pressed()
     player_step = [0, 0]
+    keys = pygame.key.get_pressed()
 
-    if keys[K_DOWN] and position.bottom != HEIGHT:
-        player_step = STEP_DOWN
+    if game_over == False:
+        bg_X1 -= BACKGROUND_STEP
+        bg_X2 -= BACKGROUND_STEP
 
-    if keys[K_UP] and position.top != 0:
-        player_step = STEP_UP
+        if bg_X1 < -BACKGROUND.get_width():
+            bg_X1 = BACKGROUND.get_width()
 
-    if keys[K_RIGHT] and position.right != WIDTH:
-        player_step = STEP_RIGHT
+        if bg_X2 < -BACKGROUND.get_width():
+            bg_X2 = BACKGROUND.get_width()
 
-    if keys[K_LEFT] and position.left != 0:
-        player_step = STEP_LEFT
+        main_display.blit(BACKGROUND, (bg_X1, 0))
+        main_display.blit(BACKGROUND, (bg_X2, 0))
+
+        if keys[K_DOWN] and position.bottom != HEIGHT:
+            player_step = STEP_DOWN
+
+        if keys[K_UP] and position.top != 0:
+            player_step = STEP_UP
+
+        if keys[K_RIGHT] and position.right != WIDTH:
+            player_step = STEP_RIGHT
+
+        if keys[K_LEFT] and position.left != 0:
+            player_step = STEP_LEFT
+
+    if game_over and keys[K_SPACE]:
+        game_over = False
+        score = 0
+        enemies = []
+        bonuses = []
+        position = player_initial_position()
 
     for enemy in enemies:
-        enemy[1] = enemy[1].move(enemy[2])
         main_display.blit(enemy[0], enemy[1])
+        if game_over:
+            continue
+
+        enemy[1] = enemy[1].move(enemy[2])
 
         if position.colliderect(enemy[1]):
             print("Game over")
-            in_progress = False
+            game_over = True
 
         if enemy[1].right < 0:
             enemies.pop(enemies.index(enemy))
 
     for bonus in bonuses:
-        bonus[1] = bonus[1].move(bonus[2])
         main_display.blit(bonus[0], bonus[1])
+        if game_over:
+            continue
+
+        bonus[1] = bonus[1].move(bonus[2])
 
         if position.colliderect(bonus[1]):
             score += 1
@@ -154,6 +168,16 @@ while in_progress:
 
     main_display.blit(FONT.render(str(score), True, COLOR_BLACK), (WIDTH - 50, 20))
     main_display.blit(player, position)
-    position = position.move(player_step)
+
+    if game_over:
+        text = FONT.render("GAME OVER! Score: " + str(score), True, COLOR_BLACK)
+        text_position = text.get_rect(center = pygame.display.get_surface().get_rect().center).move(0, -15)
+        main_display.blit(text, text_position)
+
+        text = FONT.render("Press <Space> to continue", True, COLOR_BLACK)
+        text_position = text.get_rect(center = pygame.display.get_surface().get_rect().center).move(0, 15)
+        main_display.blit(text, text_position)
+    else:
+        position = position.move(player_step)
 
     pygame.display.flip()
